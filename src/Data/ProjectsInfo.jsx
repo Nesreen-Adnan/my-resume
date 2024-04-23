b02f069d3eea5f6a66b482f866275297a09bbeb7
nesreen <nesreenad742@gmail.com>
2024-04-23 19:42:17 +0300

    add calculater app to portfolio

--- src/Data/ProjectsInfo.jsx
+++ src/Data/ProjectsInfo.jsx
@@ -3,6 +3,7 @@ import firstProject from "../assets/images/projects/first_project.png";
 import secondProject from "../assets/images/projects/second_project.png";
 import rockPaperScissors from "../assets/images/projects/rock-paper-scissors.png";
 import ageCalculator from "../assets/images/projects/age-calculator.png";
+import calculator from "../assets/images/projects/calculator.png";
 import { FaHtml5 , FaCss3Alt, FaJs, FaVuejs, FaReact, FaBootstrap} from "react-icons/fa6";
 import { SiPug, SiSass } from "react-icons/si";
 let i = 0,
@@ -18,8 +19,9 @@ let i = 0,
         {name: "rock-paper-scissors", imgSrc: rockPaperScissors, tools: [html, css, react]},
         {name: "first_project", imgSrc: firstProject, hasGitPage: true, tools: [html, css, js, bootstrap]},
         {name: "second_project", imgSrc: secondProject, hasGitPage: true, tools: [html, css, js, bootstrap]},
+        {name: "calculator", imgSrc: calculator, tools: [pug, sass, vue]},
         {name: "ageCalculator", imgSrc: ageCalculator, tools: [pug, sass, vue]},
-        {name: "memory-game-challenge23", imgSrc: memoryGame, tools: [pug, sass, vue]},
+        // {name: "memory-game-challenge23", imgSrc: memoryGame, tools: [pug, sass, vue]},
     ];
 
 export default projectsInfo;
